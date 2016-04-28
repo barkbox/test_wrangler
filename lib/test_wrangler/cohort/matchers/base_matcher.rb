c9658845e759d360350967dde7b3e22438c6b5e4
Erik Sälgström Peterson <erik@barkbox.com>
2016-04-28 12:29:55 -0400

    Add serialization and deserialization for cohorts
    Make all matchers inherit from base matcher. Implement serializer for Cohort. Implement basic serializer
    for base matcher class. Implement specific serializer for user agent matcher class. Spec out new functionality

---
+++ lib/test_wrangler/cohort/matchers/base_matcher.rb
@@ -0,0 +1,18 @@
+module TestWrangler
+  class Cohort
+    module Matchers
+      class BaseMatcher
+        attr_reader :rules
+        
+        def initialize(*rules)
+          @rules = rules.flatten
+        end
+
+        def self.deserialize(array)
+          array
+        end
+
+      end
+    end
+  end
+end
