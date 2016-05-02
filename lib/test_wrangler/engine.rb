9b5f11879d5d6e2b4a6bb6a50f114d6bc15f2c7d
Erik Sälgström Peterson <erik@barkbox.com>
2016-05-02 15:46:44 -0400

    Add middleware bootstrap to engine

--- lib/test_wrangler/engine.rb
+++ lib/test_wrangler/engine.rb
@@ -1,5 +1,10 @@
 module TestWrangler
   class Engine < ::Rails::Engine
     isolate_namespace TestWrangler
+
+    initializer "test_wrangler.add_middleware" do |app|
+      app.middleware.use TestWrangler::Middleware
+    end
+
   end
 end
