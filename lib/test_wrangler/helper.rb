b45a9baf0c9d59225ef4a8e813e969a41571e14b
Erik Sälgström Peterson <erik@barkbox.com>
2016-05-02 16:45:59 -0400

    Add spec and implementation for helper methods

---
+++ lib/test_wrangler/helper.rb
@@ -0,0 +1,21 @@
+module TestWrangler
+  module Helper
+    BLANK_SELECTION = HashWithIndifferentAccess.new({cohort: nil, experiment: nil, variant: nil})
+    
+    def test_wrangler_selection
+      return @test_wrangler_selection if defined? @test_wrangler_selection
+      if cookies['test_wrangler']
+        @test_wrangler_selection = HashWithIndifferentAccess.new(JSON.parse(cookies['test_wrangler'])) rescue BLANK_SELECTION
+      else
+        @test_wrangler_selection = BLANK_SELECTION
+      end
+    end
+
+    def complete_experiment
+      selection = test_wrangler_selection
+      cookies.delete('test_wrangler')
+      selection
+    end
+
+  end
+end
---
+++ spec/helpers/test_wrangler_helper_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+require 'test_wrangler/helper'
+require 'pry'
+
+describe TestWrangler::Helper do
+  
+  describe '#test_wrangler_selection' do
+    context 'when no selection is set in the cookies' do
+      it 'returns a blank selection' do
+        expect(helper.test_wrangler_selection).to eq({cohort: nil, experiment: nil, variant: nil}.with_indifferent_access)
+      end
+    end
+
+    context 'when a selection is set in the cookies' do
+      before do
+        @request.cookies['test_wrangler'] = JSON.generate({cohort: 'base', experiment: 'facebook_signup', variant: 'control'})
+      end
+
+      it 'returns the selection from the cookies' do
+        expect(helper.test_wrangler_selection).to eq(HashWithIndifferentAccess.new({cohort: 'base', experiment: 'facebook_signup', variant: 'control'}))
+      end
+    end
+  end
+
+  describe '#complete_experiment' do
+    context 'when no selection is set in the cookies' do
+      it 'returns a blank selection' do
+        expect(helper.complete_experiment).to eq({cohort: nil, experiment: nil, variant: nil}.with_indifferent_access)
+      end
+    end
+
+    context 'when a selection is set in the cookies' do
+      before do
+        @request.cookies['test_wrangler'] = JSON.generate({cohort: 'base', experiment: 'facebook_signup', variant: 'control'})
+      end
+      
+      it 'returns the selection' do
+        expect(helper.complete_experiment).to eq(HashWithIndifferentAccess.new({cohort: 'base', experiment: 'facebook_signup', variant: 'control'}))
+      end
+
+      it 'unsets the cookie' do
+        expect{helper.complete_experiment}.to change{helper.cookies['test_wrangler']}.to(nil)
+      end
+    end
+  end
+
+end
