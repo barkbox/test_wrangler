abf94d1c0a4ef077fb31a9ed5cd9cd5d3dd56712
Erik Sälgström Peterson <erik@barkbox.com>
2016-05-02 17:53:01 -0400

    Fix engine

--- lib/test_wrangler/engine.rb
+++ lib/test_wrangler/engine.rb
@@ -2,11 +2,8 @@ module TestWrangler
   class Engine < ::Rails::Engine
     isolate_namespace TestWrangler
 
-    initializer "test_wrangler.add_middleware" do |app|
+    initializer "test_wrangler" do |app|
       app.middleware.use TestWrangler::Middleware
-    end
-
-    intializer "test_wrangler.bootstrap_helper" do |app|
       ActionController::Base.send :include, TestWrangler::Helper
       ActionController::Base.helper TestWrangler::Helper
     end
