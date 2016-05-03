50f58feb15f2e75444ed2ad0976e5d356d5b82d8
Erik Sälgström Peterson <erik@barkbox.com>
2016-05-03 13:37:32 -0400

    Switch up require order

--- lib/test_wrangler/cohort/matchers.rb
+++ lib/test_wrangler/cohort/matchers.rb
@@ -1,10 +1,11 @@
-Dir[File.expand_path('../matchers/*_matcher.rb', __FILE__)].each do |f|
-  require f
-end
-
 module TestWrangler
   class Cohort
     module Matchers
     end
   end
 end
+
+Dir[File.expand_path('../matchers/*_matcher.rb', __FILE__)].sort.each do |f|
+  require f
+end
+
