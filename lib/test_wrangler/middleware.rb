4a0d121dc22349f5d6780a332c0010d499f8f91f
Erik Sälgström Peterson <erik@barkbox.com>
2016-05-03 18:00:54 -0400

    Make sure to unescape cookie value

--- lib/test_wrangler/middleware.rb
+++ lib/test_wrangler/middleware.rb
@@ -9,8 +9,8 @@ module TestWrangler
     def call(env)
       return app.call(env) unless TestWrangler.active? && TestWrangler.valid_request_path?(env['REQUEST_PATH'])
 
-      req = Rack::Request.new(env)
-      tw_cookie = JSON.parse(req.cookies['test_wrangler']).with_indifferent_access rescue nil
+      req = ActionDispatch::Request.new(env)
+      tw_cookie = JSON.parse(Rack::Utils.unescape(req.cookies['test_wrangler'])).with_indifferent_access rescue nil
 
       if tw_cookie && TestWrangler.experiment_active?(tw_cookie['experiment'])
         env['test_wrangler'] = tw_cookie
