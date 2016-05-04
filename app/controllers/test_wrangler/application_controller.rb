e7f6d56d2a8d7573440614a44f2fdd6c773eb8c2
Erik Sälgström Peterson <erik@barkbox.com>
2016-05-04 13:43:33 -0400

    Add spec and implementation for ApplicationController auth

--- app/controllers/test_wrangler/application_controller.rb
+++ app/controllers/test_wrangler/application_controller.rb
@@ -1,5 +1,6 @@
 module TestWrangler
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
+    http_basic_authenticate_with name: TestWrangler.config.username, password: TestWrangler.config.password
   end
 end
