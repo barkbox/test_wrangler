07ce880eebcc496e0fc6944685c06f3d55ac8631
Erik Sälgström Peterson <erik@barkbox.com>
2016-05-16 10:47:56 -0400

    Fix HTTP Auth

--- app/controllers/test_wrangler/application_controller.rb
+++ app/controllers/test_wrangler/application_controller.rb
@@ -1,12 +1,12 @@
 module TestWrangler
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
-    before_action :http_auth
+    before_filter :http_auth
 
     def http_auth
       creds = ActionController::HttpAuthentication::Basic.decode_credentials(request) rescue nil
       return if creds == "#{TestWrangler.config.username}:#{TestWrangler.config.password}"
-      render nothing: true, status: :unauthorized
+      request_http_basic_authentication
     end
 
   end
