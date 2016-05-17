2046a597f573def726cd3ef9ffc2e34b3bd831a5
Erik Sälgström Peterson <erik@barkbox.com>
2016-05-17 11:23:13 -0400

    Change to PUT requests for backbone

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,14 @@ TestWrangler::Engine.routes.draw do
     get 'experiments' => 'experiments#index'
     get 'experiments/:experiment_name' => 'experiments#show'
     post 'experiments/:experiment_name' => 'experiments#update'
+    put 'experiments/:experiment_name' => 'experiments#update'
     post 'experiments' => 'experiments#create'
     delete 'experiments/:experiment_name' => 'experiments#destroy'
 
     get 'cohorts' => 'cohorts#index'
     get 'cohorts/:cohort_name' => 'cohorts#show'
     post 'cohorts/:cohort_name' => 'cohorts#update'
+    put 'cohorts/:cohort_name' => 'cohorts#update'
     post 'cohorts' => 'cohorts#create'
     delete 'cohorts/:cohort_name' => 'cohorts#destroy'
   end
