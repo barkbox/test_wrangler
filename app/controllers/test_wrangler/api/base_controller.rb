46347f0c9f5ba146a791aedf4c92908e35146ba7
Erik Sälgström Peterson <erik@barkbox.com>
2016-05-17 11:23:42 -0400

    Skip CSRF token on API controller

--- app/controllers/test_wrangler/api/base_controller.rb
+++ app/controllers/test_wrangler/api/base_controller.rb
@@ -1,4 +1,5 @@
 class TestWrangler::Api::BaseController < TestWrangler::ApplicationController
+  skip_before_filter :verify_authenticity_token
   before_filter :ensure_json_request  
 
   def ensure_json_request  
