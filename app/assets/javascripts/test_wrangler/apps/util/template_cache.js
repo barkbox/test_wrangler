2b61a7645e7c1ff4454853f986b26b1418a80628
Erik Sälgström Peterson <eriksalgstrom@gmail.com>
2016-05-19 17:43:50 -0400

    Asset Fixes (#2)
    * Try for engine initializer fix
    
    * Try adding template path
    
    * Trying something
    
    * Trying something
    
    * Change include

--- app/assets/javascripts/test_wrangler/apps/util/template_cache.js
+++ app/assets/javascripts/test_wrangler/apps/util/template_cache.js
@@ -3,7 +3,7 @@ var TemplateCache = (function(){
     return {
         get: function(name){
             if(cache[name]) return $.Deferred().resolve(cache[name]);
-            return $.get('/assets/test_wrangler/' + name + '.dot')
+            return $.get('/assets/templates/test_wrangler/' + name + '.dot')
                         .then(function(file){
                             cache[name] = doT.template(file);
                             return cache[name];
--- lib/test_wrangler/engine.rb
+++ lib/test_wrangler/engine.rb
@@ -2,14 +2,22 @@ module TestWrangler
   class Engine < ::Rails::Engine
     isolate_namespace TestWrangler
 
-    initializer "test_wrangler" do |app|
+    initializer "test_wrangler.middlware" do |app|
       app.middleware.use TestWrangler::Middleware
+    end
+
+    initializer "test_wrangler.helpers" do |app|
       ActionController::Base.send :include, TestWrangler::Helper
       ActionController::Base.helper TestWrangler::Helper
       ActionController::Base.helper_method :test_wrangler_selection, :complete_experiment
-
-      app.config.assets.precompile += %w(test_wrangler.js test_wrangler.css)
     end
-    
+
+    initializer "test_wrangler.precompile", group: :all do |app|
+      app.config.assets.precompile += ["test_wrangler/test_wrangler.js", "test_wrangler/test_wrangler.css"]
+    end
+
+    initializer "test_wrangler.static" do |app|
+      app.middleware.use(::ActionDispatch::Static, "#{root}/public")
+    end
   end
 end
