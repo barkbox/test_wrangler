eae491c8b58cc7eee21986b77bec76e737018615
Erik Sälgström Peterson <erik@barkbox.com>
2016-05-19 17:20:26 -0400

    Trying something

--- lib/test_wrangler/engine.rb
+++ lib/test_wrangler/engine.rb
@@ -4,17 +4,20 @@ module TestWrangler
 
     initializer "test_wrangler.middlware" do |app|
       app.middleware.use TestWrangler::Middleware
-      ActionController::Base.helper_method :test_wrangler_selection, :complete_experiment
     end
 
     initializer "test_wrangler.helpers" do |app|
       ActionController::Base.send :include, TestWrangler::Helper
       ActionController::Base.helper TestWrangler::Helper
+      ActionController::Base.helper_method :test_wrangler_selection, :complete_experiment
     end
 
     initializer "test_wrangler.precompile", group: :all do |app|
-      app.config.assets.paths << root.join("app","assets","templates")
       app.config.assets.precompile += ["test_wrangler/test_wrangler.js", "test_wrangler/test_wrangler.css"]
+    end
+
+    initializer "test_wrangler.static" do |app|
+      app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public")
     end
   end
 end
