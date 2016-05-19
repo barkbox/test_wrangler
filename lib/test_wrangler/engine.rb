3cf0a78c0a68c8e9a80f01a2bceb1bd3c3bf7dcb
Erik Sälgström Peterson <erik@barkbox.com>
2016-05-19 16:47:40 -0400

    Try for engine initializer fix

--- lib/test_wrangler/engine.rb
+++ lib/test_wrangler/engine.rb
@@ -2,14 +2,20 @@ module TestWrangler
   class Engine < ::Rails::Engine
     isolate_namespace TestWrangler
 
-    initializer "test_wrangler" do |app|
+    initializer "test_wrangler.middlware" do |app|
       app.middleware.use TestWrangler::Middleware
+      ActionController::Base.helper_method :test_wrangler_selection, :complete_experiment
+    end
+
+    initializer "test_wrangler.helpers" do |app|
       ActionController::Base.send :include, TestWrangler::Helper
       ActionController::Base.helper TestWrangler::Helper
-      ActionController::Base.helper_method :test_wrangler_selection, :complete_experiment
-
-      app.config.assets.precompile += %w(test_wrangler.js test_wrangler.css)
     end
-    
+
+    initializer "test_wrangler.precompile", group: :all do |app|
+      # app.config.assets.paths << root.join("app","assets","stylesheets", "test_wrangler")
+      # app.config.assets.paths << root.join("app","assets","javascripts", "test_wrangler")
+      app.config.assets.precompile += ["test_wrangler/test_wrangler.js", "test_wrangler/test_wrangler.css"]
+    end
   end
 end
